4b43db3343e3267374f7d4d9d6314a24e4d504c9
Maru Maruyama <maruyama@zukat.jp>
2021-10-31 23:12:54 +0900

    Added x86 build with latest Unity 2020 LTS

---
+++ APK/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,111 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BackgroundController::Start()
+extern void BackgroundController_Start_m5436404692240F761DFBF2E9E5AF7E404EB03AF1 (void);
+// 0x00000002 System.Void BackgroundController::Update()
+extern void BackgroundController_Update_m689300F57C28C8BA376D8528BFADAC8F5F964087 (void);
+// 0x00000003 System.Void BackgroundController::.ctor()
+extern void BackgroundController__ctor_m4B15F2D9DD3BA08F551F8D8C0B70D267FBCDCB0B (void);
+// 0x00000004 System.Void CubeController::Start()
+extern void CubeController_Start_m651A2FE374AA3DD64309E29CC2995FCCBB52893B (void);
+// 0x00000005 System.Void CubeController::Update()
+extern void CubeController_Update_mB8390358157270FB179FAF4BF7B5B9BBC06DF2A9 (void);
+// 0x00000006 System.Void CubeController::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void CubeController_OnCollisionEnter2D_mB8302335D5723B51A856A8777DC2BB4AE9A2C2E1 (void);
+// 0x00000007 System.Void CubeController::.ctor()
+extern void CubeController__ctor_mD00619AE644B6F6052B58788552C92E247F8C5A4 (void);
+// 0x00000008 System.Void CubeGenerator::Start()
+extern void CubeGenerator_Start_m224F07F8C00149052B8AD4A44219EFC9CB1D6904 (void);
+// 0x00000009 System.Void CubeGenerator::Update()
+extern void CubeGenerator_Update_mF8F99A424635B8551FE41847FB9E86DA8D1C0D97 (void);
+// 0x0000000A System.Void CubeGenerator::.ctor()
+extern void CubeGenerator__ctor_m4EC04174E9CE97C0B19699468E18DD63B4F9FC6A (void);
+// 0x0000000B System.Void UIController::Start()
+extern void UIController_Start_m74C43E03145ECBC4566A9979DB7A1D342972A527 (void);
+// 0x0000000C System.Void UIController::Update()
+extern void UIController_Update_m01A435E125093AE9631843DBDD904FEF5C82D282 (void);
+// 0x0000000D System.Void UIController::GameOver()
+extern void UIController_GameOver_mEB8D877C49870F094CDBCECBE3DAC3441913D51A (void);
+// 0x0000000E System.Void UIController::.ctor()
+extern void UIController__ctor_m57DAF9FADDD58C79489BCF6474E55D3514E9BA21 (void);
+// 0x0000000F System.Void UnityChanController::Start()
+extern void UnityChanController_Start_m21A3F3DC8A032ABB04BB988395EA2CBAAFA65981 (void);
+// 0x00000010 System.Void UnityChanController::Update()
+extern void UnityChanController_Update_m578E889847EB3A881F1303EB4D40D54C3B77A98F (void);
+// 0x00000011 System.Void UnityChanController::.ctor()
+extern void UnityChanController__ctor_m49B109B81EFF9A9867262BA79B4AA5AD54145B7B (void);
+static Il2CppMethodPointer s_methodPointers[17] = 
+{
+	BackgroundController_Start_m5436404692240F761DFBF2E9E5AF7E404EB03AF1,
+	BackgroundController_Update_m689300F57C28C8BA376D8528BFADAC8F5F964087,
+	BackgroundController__ctor_m4B15F2D9DD3BA08F551F8D8C0B70D267FBCDCB0B,
+	CubeController_Start_m651A2FE374AA3DD64309E29CC2995FCCBB52893B,
+	CubeController_Update_mB8390358157270FB179FAF4BF7B5B9BBC06DF2A9,
+	CubeController_OnCollisionEnter2D_mB8302335D5723B51A856A8777DC2BB4AE9A2C2E1,
+	CubeController__ctor_mD00619AE644B6F6052B58788552C92E247F8C5A4,
+	CubeGenerator_Start_m224F07F8C00149052B8AD4A44219EFC9CB1D6904,
+	CubeGenerator_Update_mF8F99A424635B8551FE41847FB9E86DA8D1C0D97,
+	CubeGenerator__ctor_m4EC04174E9CE97C0B19699468E18DD63B4F9FC6A,
+	UIController_Start_m74C43E03145ECBC4566A9979DB7A1D342972A527,
+	UIController_Update_m01A435E125093AE9631843DBDD904FEF5C82D282,
+	UIController_GameOver_mEB8D877C49870F094CDBCECBE3DAC3441913D51A,
+	UIController__ctor_m57DAF9FADDD58C79489BCF6474E55D3514E9BA21,
+	UnityChanController_Start_m21A3F3DC8A032ABB04BB988395EA2CBAAFA65981,
+	UnityChanController_Update_m578E889847EB3A881F1303EB4D40D54C3B77A98F,
+	UnityChanController__ctor_m49B109B81EFF9A9867262BA79B4AA5AD54145B7B,
+};
+static const int32_t s_InvokerIndices[17] = 
+{
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	969,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	17,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
